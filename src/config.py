d239ad6c7d45a43cdea2fa29089eeccc076479d1
Kishor978 <thaunnakishor@gmail.com>
2025-07-09 20:02:01 +0545

    updated log for kaggle

--- src/config.py
+++ src/config.py
@@ -1,24 +1,33 @@
-# src/config.py
+import os
 
 # Dataset Paths
 # VIDGEN_DATASET_PATH = r"E:\Cyberbullying\dataset\raw\Dynamically Generated Hate Dataset v0.2.2.csv"
 # DAVIDSON_DATASET_PATH = r"E:\Cyberbullying\dataset\raw\davidson.csv"
 # HATEMOJI_VALIDATION_PATH = r"E:\Cyberbullying\dataset\raw\HatemojiBuild\validation.csv"
 
-
-VIDGEN_DATASET_PATH = "/kaggle/input/cyberbullying/raw/Dynamically Generated Hate Dataset v0.2.2.csv"
-DAVIDSON_DATASET_PATH = "/kaggle/input/cyberbullying/raw/davidson-dataset/davidson.csv"
-HATEMOJI_VALIDATION_PATH = "/kaggle/input/cyberbullying/raw/hatemojibuild/validation.csv"
-
 # Model and Output Paths
-LOGISTIC_REGRESSION_MODEL_PATH = './models/logistic_regression_model.pkl'
-BERT_MODEL_OUTPUT_DIR = './results/bert_model'
-BILSTM_MODEL_OUTPUT_DIR = './results/bilstm_model'
-EMOTION_FUSION_MODEL_OUTPUT_DIR = './results/emotion_fusion_model'
+# LOGISTIC_REGRESSION_MODEL_PATH = './models/logistic_regression_model.pkl'
+# BERT_MODEL_OUTPUT_DIR = './results/bert_model'
+# BILSTM_MODEL_OUTPUT_DIR = './results/bilstm_model'
+# EMOTION_FUSION_MODEL_OUTPUT_DIR = './results/emotion_fusion_model'
 
 # GloVe Embedding Path (for BiLSTM)
 # GLOVE_PATH = r"E:\Cyberbullying\glove\glove.6B.100d.txt" # Adjust this path
+
+KAGGLE_WORKING_DIR = "/kaggle/working"
+VIDGEN_DATASET_PATH = "/kaggle/input/cyberbullying/raw/Dynamically Generated Hate Dataset v0.2.2.csv"
+DAVIDSON_DATASET_PATH = "/kaggle/input/cyberbullying/raw/davidson-dataset/davidson.csv"
+HATEMOJI_VALIDATION_PATH = "/kaggle/input/cyberbullying/raw/hatemojibuild/validation.csv"
 GLOVE_PATH= "/kaggle/input/cyberbullying/glove.6B.100d.txt"  # Adjust this path
+
+LOGISTIC_REGRESSION_MODEL_PATH = os.path.join(KAGGLE_WORKING_DIR, 'models', 'logistic_regression_model.pkl')
+BERT_MODEL_OUTPUT_DIR = os.path.join(KAGGLE_WORKING_DIR, 'results', 'bert_model')
+BILSTM_MODEL_OUTPUT_DIR = os.path.join(KAGGLE_WORKING_DIR, 'results', 'bilstm_model')
+EMOTION_FUSION_MODEL_OUTPUT_DIR = os.path.join(KAGGLE_WORKING_DIR, 'results', 'emotion_fusion_model')
+
+# Log directory for BERT Trainer
+LOGGING_DIR = os.path.join(KAGGLE_WORKING_DIR, 'logs')
+
 # General Settings
 TFIDF_MAX_FEATURES = 5000
 RANDOM_STATE = 42
